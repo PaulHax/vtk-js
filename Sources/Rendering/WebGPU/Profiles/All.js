93cfdb63235ef47449a5955253ea0d59feaba2ad
Adnane Belmadiaf <adnane002@gmail.com>
2026-04-04 16:41:04 +0200

    feat(ImageCPRMapper): add ImageCPRMapper in WebGPU

--- Sources/Rendering/WebGPU/Profiles/All.js
+++ Sources/Rendering/WebGPU/Profiles/All.js
@@ -18,6 +18,7 @@ import 'vtk.js/Sources/Rendering/WebGPU/Glyph3DMapper';
 
 // Volume
 import 'vtk.js/Sources/Rendering/WebGPU/ImageMapper';
+import 'vtk.js/Sources/Rendering/WebGPU/ImageCPRMapper';
 import 'vtk.js/Sources/Rendering/WebGPU/ImageSlice';
 import 'vtk.js/Sources/Rendering/WebGPU/Volume';
 // import 'vtk.js/Sources/Rendering/WebGPU/VolumeMapper';
